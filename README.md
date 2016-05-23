fdbacd080d8e1143714ae6ca468636124f4b74aa
Kush Agrawal <Kush@Kushs-MacBook-Air.local>
2016-05-23 00:35:18 -0700

    Changed website

---
+++ README.md
@@ -0,0 +1,2 @@
+# rgb2015
+Website for Innovative Design RGB 2015
--- startup/common-files/js/rotate.js
+++
@@ -1,9 +0,0 @@
-var terms = ["Beauticians", "Electricians", "Plumbers  ", "Carpenters", "Contractors"];
-
-function rotateTerm() {
-  var ct = $("#rotate").data("term") || 0;
-  $("#rotate").data("term", ct == terms.length -1 ? 0 : ct + 1).text(terms[ct]).fadeIn()
-              .delay(2000).fadeOut(200, rotateTerm);
-}
-$(rotateTerm);
-
